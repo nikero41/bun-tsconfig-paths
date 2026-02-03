c7e2aa25f82e199eb88f780a4cc65006e2de8ab4
Stavros Nikoletopoulos <snikoletopoulos@hotmail.com>
2026-02-03 22:10:25 +0100

    Create plugin

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import type { BunPlugin } from "bun";
+import tsConfigPaths from "tsconfig-paths";
+
+export interface TsConfigPathsOptions {
+	external?: boolean;
+}
+
+const tsconfigPaths = (options: TsConfigPathsOptions = {}): BunPlugin => ({
+	name: "bun-tsconfig-paths",
+	setup(build) {
+		const result = tsConfigPaths.loadConfig();
+		if (result.resultType === "failed") return;
+
+		const matchPath = tsConfigPaths.createMatchPath(
+			result.absoluteBaseUrl,
+			result.paths,
+		);
+
+		// const tsConfigAlliasRegExp = new RegExp(
+		// 	"^" +
+		// 		Object.keys(result.paths)
+		// 			.map(p => (p === "*" ? "^\\w+" : p.replace("*", "")))
+		// 			.join("|"),
+		// );
+
+		build.onResolve({ filter: /.*/, namespace: "file" }, args => {
+			const newAbsolutePath = matchPath(args.path.replace(/\.js$/, ".ts"));
+			if (!newAbsolutePath) return;
+			return {
+				path: newAbsolutePath,
+				...("external" in options ? { external: options.external } : {}),
+			};
+		});
+	},
+});
+
+export default tsconfigPaths;
